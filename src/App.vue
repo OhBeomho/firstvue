c7ce11bc8159649b772259feef316ea0a5d348a7
OhBeomho <tyler0322@naver.com>
2022-04-17 18:41:22 +0900

    abc

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,51 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
+  </div>
+
+  <div>
+    <h1>Games</h1>
+  </div>
+  <div class="games">
+    <div v-for="(game, i) in games" :key="i" class="game">
+      <h3>{{ game.name }}</h3>
+      <p>Price: &#8361; {{ game.price }}</p>
+      <button @click="report(i)">허위게임신고</button> <span>신고수: {{ game.report }}</span>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
+  data() {
+    return {
+      games: [
+        {
+          name: "Roblox",
+          price: 0,
+          report: 0
+        },
+        {
+          name: "Geometry Dash",
+          price: 4400,
+          report: 0
+        },
+        {
+          name: "Minecraft",
+          price: 30000,
+          report: 0
+        }
+      ],
+      menus: [ "Home", "Shop", "About" ]
+    };
+  },
+  methods: {
+    report(index) {
+      this.games[index].report++;
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -21,6 +57,54 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+
+.menu a {
+  color: white;
+  padding: 10px;
+}
+
+.games {
+  padding: 20px;
+  align-items: center;
+  justify-content: center;
+}
+
+.game {
+  width: 80%;
+
+  border-radius: 3px;
+  background: lightgray;
+
+  padding-bottom: 20px;
+  padding-top: 2px;
+
+  margin: auto;
+  margin-bottom: 20px;
+
+  justify-content: center;
+  align-items: center;
+}
+
+.game button {
+  background: darkslateblue;
+  border: 0;
+  border-radius: 5px;
+  padding: 5px;
+  color: white;
+}
+
+.game button:hover {
+  background: rgb(96, 81, 192);
+}
+
+.game button:active {
+  background: rgb(43, 31, 125);
 }
 </style>
