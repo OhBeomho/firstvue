c4dab5c35b30487d4e9c43c4493f598572ff6043
OhBeomho <tyler0322@naver.com>
2022-04-19 18:34:22 +0900

    모달창

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <div class="black-bg">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+    </div>
+  </div>
+
   <div class="menu">
     <a v-for="(menu, i) in menus" :key="i">{{ menu }}</a>
   </div>
@@ -8,6 +15,7 @@
   </div>
   <div class="games">
     <div v-for="(game, i) in games" :key="i" class="game">
+      <img :src="game.image" alt="Image">
       <h3>{{ game.name }}</h3>
       <p>Price: &#8361; {{ game.price }}</p>
       <button @click="report(i)">허위게임신고</button> <span>신고수: {{ game.report }}</span>
@@ -16,26 +24,21 @@
 </template>
 
 <script>
+function GameObject(name, price) {
+  this.name = name;
+  this.price = price;
+  this.report = 0;
+  this.image = require(`./assets/${name}.png`);
+}
+
 export default {
   name: 'App',
   data() {
     return {
       games: [
-        {
-          name: "Roblox",
-          price: 0,
-          report: 0
-        },
-        {
-          name: "Geometry Dash",
-          price: 4400,
-          report: 0
-        },
-        {
-          name: "Minecraft",
-          price: 30000,
-          report: 0
-        }
+        new GameObject("Roblox", "free"),
+        new GameObject("Geometry Dash", "4400"),
+        new GameObject("Minecraft", "30000")
       ],
       menus: [ "Home", "Shop", "About" ]
     };
@@ -57,6 +60,29 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+body {
+  margin: 0px;
+}
+
+div {
+  box-sizing: border-box;
+}
+
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 5px;
+  padding: 20px;
 }
 
 .menu {
@@ -83,13 +109,18 @@ export default {
   background: lightgray;
 
   padding-bottom: 20px;
-  padding-top: 2px;
+  padding-top: 20px;
 
   margin: auto;
   margin-bottom: 20px;
 
   justify-content: center;
   align-items: center;
+}
+
+.game img {
+  width: 120px;
+  border-radius: 10px;
 }
 
 .game button {
