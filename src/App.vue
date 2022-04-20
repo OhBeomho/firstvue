9798c625843684dd778a35a100b75ff0b00b30c7
OhBeomho <tyler0322@naver.com>
2022-04-20 17:05:45 +0900

    description

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="black-bg" v-if="dialogVisible == true">
+  <div class="black-bg" v-if="dialog.visible == true">
     <div class="white-bg">
-      <h4 ref="dialogTitle">상세페이지</h4>
-      <p ref="dialogContent">상세페이지 내용</p>
-      <button @click="setDialogState(false)">닫기</button>
+      <h4>{{ dialog.title }}</h4>
+      <p>{{ dialog.content }}</p>
+      <button @click="dialog.visible = false">닫기</button>
     </div>
   </div>
 
@@ -17,7 +17,7 @@
   <div class="games">
     <div v-for="(game, i) in games" :key="i" class="game">
       <img :src="game.image" alt="Image">
-      <h3 @click="setDialogState(game.name, '', true)">{{ game.name }}</h3>
+      <h3 @click="setDialogState(game.name, game.desc, true)">{{ game.name }}</h3>
       <p>가격: &#8361; {{ game.price }}</p>
       <button @click="report(i)">허위게임신고</button> <span>신고수: {{ game.report }}</span>
     </div>
@@ -33,27 +33,38 @@ function GameObject(name, price, desc) {
   this.image = require(`./assets/${name}.png`);
 }
 
+function Dialog() {
+  this.title = "";
+  this.content = "";
+  this.visible = false;
+}
+
 export default {
   name: 'App',
   data() {
     return {
       games: [
-        new GameObject("Roblox", "free", ""),
-        new GameObject("Geometry Dash", "4400", ""),
-        new GameObject("Minecraft", "30000", "")
+        new GameObject("Roblox", "free", `2004년 데이비드 바수츠키와 에릭 카셀(Erik Cassel)이 설립한 Roblox Corporation에서 만든 메타버스 게임 플랫폼.
+        사용자들이 직접 게임을 만들고, 플레이할 수 있다.
+        그래서 로블록스 자체는 게임보다는 플랫폼에 가깝다.`),
+        new GameObject("Geometry Dash", "4400", `스웨덴 국적인 로버트 토팔라 RobTop(Robert Topala)이
+        운영중인 1인 회사 RobTopGames에서 2013년 8월 13일에 출시한
+        iOS, Android, Windows Phone용 리듬 기반 액션 플랫포밍 유료 게임.`),
+        new GameObject("Minecraft", "30000", `Mojang(모장) 스튜디오에서 2009년 처음 발매한 샌드박스 형식의 비디오 게임.
+        이름처럼 채광(Mine)과 제작(Craft)을 하는 게임으로 흔히 알려져 있다.`)
       ],
       menus: [ "Home", "Shop", "About" ],
-      dialogVisible: false
+      dialog: new Dialog()
     };
   },
   methods: {
-    report(index) {
+    report: function(index) {
       this.games[index].report++;
     },
-    setDialogState(title = "", content = "", visible) {
-      this.$refs.dialogTitle.innerText = title;
-      this.$refs.dialogContent.innerText = content;
-      this.dialogVisible = visible;
+    setDialogState: function(title, content, visible) {
+      this.dialog.title = title;
+      this.dialog.content = content;
+      this.dialog.visible = visible;
     }
   },
   components: {
