5a6715f376410a57a7fae35cc9cf7110aac04a2c
OhBeomho <tyler0322@naver.com>
2022-04-22 19:55:40 +0900

    added gamedata.js

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
   <div class="games">
     <div v-for="(game, i) in games" :key="i" class="game">
       <img :src="game.image" alt="Image">
-      <h3 @click="setDialogState(game.name, game.desc, true)">{{ game.name }}</h3>
+      <h3 @click="setDialogState(game.name, game.content, true)">{{ game.name }}</h3>
       <p>가격: &#8361; {{ game.price }}</p>
       <button @click="report(i)">허위게임신고</button> <span>신고수: {{ game.report }}</span>
     </div>
@@ -25,13 +25,7 @@
 </template>
 
 <script>
-function GameObject(name, price, desc) {
-  this.name = name;
-  this.price = price;
-  this.desc = desc;
-  this.report = 0;
-  this.image = require(`./assets/${name}.png`);
-}
+import gameData from "./data/gamedata.js";
 
 function Dialog() {
   this.title = "";
@@ -43,16 +37,7 @@ export default {
   name: 'App',
   data() {
     return {
-      games: [
-        new GameObject("Roblox", "free", `2004년 데이비드 바수츠키와 에릭 카셀(Erik Cassel)이 설립한 Roblox Corporation에서 만든 메타버스 게임 플랫폼.
-        사용자들이 직접 게임을 만들고, 플레이할 수 있다.
-        그래서 로블록스 자체는 게임보다는 플랫폼에 가깝다.`),
-        new GameObject("Geometry Dash", "4400", `스웨덴 국적인 로버트 토팔라 RobTop(Robert Topala)이
-        운영중인 1인 회사 RobTopGames에서 2013년 8월 13일에 출시한
-        iOS, Android, Windows Phone용 리듬 기반 액션 플랫포밍 유료 게임.`),
-        new GameObject("Minecraft", "30000", `Mojang(모장) 스튜디오에서 2009년 처음 발매한 샌드박스 형식의 비디오 게임.
-        이름처럼 채광(Mine)과 제작(Craft)을 하는 게임으로 흔히 알려져 있다.`)
-      ],
+      games: gameData,
       menus: [ "Home", "Shop", "About" ],
       dialog: new Dialog()
     };
---
+++ src/data/gamedata.js
@@ -0,0 +1,31 @@
+export default [
+	{
+		id: 0,
+		name: "Geometry Dash",
+		image: "https://github.com/OhBeomho/firstvue/blob/master/src/assets/Geometry%20Dash.png?raw=true",
+		content: `스웨덴 국적인 로버트 토팔라 RobTop(Robert Topala)이
+        운영중인 1인 회사 RobTopGames에서 2013년 8월 13일에 출시한
+        iOS, Android, Windows Phone용 리듬 기반 액션 플랫포밍 유료 게임.`,
+		price: 4400,
+		report: 0
+	},
+	{
+		id: 1,
+		name: "Roblox",
+		image: "https://github.com/OhBeomho/firstvue/blob/master/src/assets/Roblox.png?raw=true",
+		content: `2004년 데이비드 바수츠키와 에릭 카셀(Erik Cassel)이 설립한 Roblox Corporation에서 만든 메타버스 게임 플랫폼.
+        사용자들이 직접 게임을 만들고, 플레이할 수 있다.
+        그래서 로블록스 자체는 게임보다는 플랫폼에 가깝다.`,
+		price: 0,
+		report: 0
+	},
+	{
+		id: 2,
+		name: "Minecraft",
+		image: "https://github.com/OhBeomho/firstvue/blob/master/src/assets/Minecraft.png?raw=true",
+		content: `Mojang(모장) 스튜디오에서 2009년 처음 발매한 샌드박스 형식의 비디오 게임.
+        이름처럼 채광(Mine)과 제작(Craft)을 하는 게임으로 흔히 알려져 있다.`,
+		price: 30000,
+		report: 0
+	}
+]
